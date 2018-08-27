669b68e2666f29bfd53b5023ad17a9ae753f9af0
JohnMonico6 <jmonico96@gmail.com>
2018-08-27 15:35:58 +1000

    Cards Added to Pile

--- BirdWorld/src/GameLogic/Pile.java
+++ BirdWorld/src/GameLogic/Pile.java
@@ -16,15 +16,77 @@ public class Pile {
 	public Pile() {
 		pile = new ArrayList<Card>();
 		
+		
+		//CURRENTLY ALL BIRD CARDS ARE HARDCODED FOR MVP
 		int[] birdAtr = {1, 2, 3, 4, 5};
 		
 		Card wattleBird = new Card("Wattle Bird", birdAtr, "images/WattleBird.png");
 		Card wedgeTailEagle = new Card("Wedge-Tail Eagle", birdAtr, "images/WedgeTailEagle.png");
 		Card easternBristleBird = new Card("Eastern Bristle Bird", birdAtr, "images/EasternBristleBird.png");
+		Card australianMagpie = new Card("Australian Magpie", birdAtr, "images/Australian Magpie.PNG");
+		Card budgerigar = new Card("Budgerigar", birdAtr, "images/Budgerigar");
+		Card bushTurkey = new Card("Bush Turkey", birdAtr, "images/Bush Turkey.PNG");
+		Card bushStoneTurkey = new Card("Bush-Stone Curlew", birdAtr, "images/Bush Turkey.PNG");
+		Card channelBilledCuckoo = new Card("Channel-Billed Cuckoo", birdAtr, "images/Channel-Billed Cuckoo.PNG");
+		Card commonKoel = new Card("Common Koel", birdAtr, "images/Common Koel.PNG");
+		Card commonMyna = new Card("Common Myna", birdAtr, "images/Common Myna.PNG");
+		Card crestedPidgeon = new Card("Crested Pidgeon", birdAtr, "images/Crested Pidgeon");
+		Card crimsonRosella = new Card("Crimson Rosella", birdAtr, "images/Crimson Rosella");
+		Card easternWhipbird = new Card("Eastern Whipbird", birdAtr, "images/Eastern Whipbird.png");
+		Card galah = new Card ("Galah", birdAtr, "images/Galah");
+		Card guidainFinch = new Card("Gouldain Finch", birdAtr, "images/Gouldian Finch");
+		Card greyButcherbird = new Card("Grey Bitcherbird", birdAtr, "images/Grey Butcherbird.PNG");
+		Card hoodedPlover = new Card("Hooded Plover", birdAtr, "images/Hooded Plover");
+		Card kookaburra = new Card("Kookaburra", birdAtr, "images/Kookaburra");
+		Card magpieLark = new Card("Magpie Lark", birdAtr, "images/Magpie Lark.PNG");
+		Card noisyMiner = new Card("Noisy Miner", birdAtr, "images/Noisy Minder.PNG");
+		Card rainbowBeeEater = new Card("Rainbow Bee-Eater", birdAtr, "images/Rainbow Bee-Eater.PNG");
+		Card rainbowLorikeet = new Card("Rainbow Lorikeet", birdAtr, "images/Rainbow Lorikeet");
+		Card satinBowerBird = new Card("Satin Bowerbird", birdAtr, "images/Satin Bowerbird.PNG");
+		Card scarletRobin = new Card("Scarlet Robin", birdAtr, "images/Scarlet Robin.PNG");
+		Card shortTailedShearwater = new Card("Short Tailed Shearwater", birdAtr, "images/Short-Tailed Shearwater");
+		Card spurWingedPlover = new Card("Spur-Winged Plover", birdAtr, "images/Spur-Winged Plover.PNG");
+		Card suburbFairywren = new Card("Suburn Fairy Wren", birdAtr,"images/Suberd Fairywren.PNG");
+		Card sulphurCrestedCockatoo = new Card("Sulphur Crested Cockatoo", birdAtr, "Sulphur Crested Cockatoo.PNG");
+		Card superbLurebird = new Card("Superb Lurebird", birdAtr, "Superb Lurebird.PNG");
+		Card tawnyFrogmouth = new Card("Tawny Frogmouth", birdAtr, "Tawn Frogmouth.PNG");
+		Card torresianCrow = new Card("Torresian Crow", birdAtr, "Torresian Crow.PNG");
+		Card welcomeSwallow = new Card("Welcome Swallow", birdAtr, "Welcome Swallow.PNG");
 		
+		//Adding all cards to the pile
 		pile.add(wattleBird);
 		pile.add(easternBristleBird);
 		pile.add(wedgeTailEagle);
+		pile.add(australianMagpie);
+		pile.add(budgerigar);
+		pile.add(bushTurkey);
+		pile.add(bushStoneTurkey);
+		pile.add(channelBilledCuckoo);
+		pile.add(commonKoel);
+		pile.add(commonMyna);
+		pile.add(crestedPidgeon);
+		pile.add(crimsonRosella);
+		pile.add(easternWhipbird);
+		pile.add(galah);
+		pile.add(guidainFinch);
+		pile.add(greyButcherbird);
+		pile.add(hoodedPlover);
+		pile.add(kookaburra);
+		pile.add(magpieLark);
+		pile.add(noisyMiner);
+		pile.add(rainbowBeeEater);
+		pile.add(rainbowLorikeet);
+		pile.add(satinBowerBird);
+		pile.add(scarletRobin);
+		pile.add(shortTailedShearwater);
+		pile.add(spurWingedPlover);
+		pile.add(suburbFairywren);
+		pile.add(sulphurCrestedCockatoo);
+		pile.add(superbLurebird);
+		pile.add(tawnyFrogmouth);
+		pile.add(torresianCrow);
+		pile.add(welcomeSwallow);
+		
 	}
 	
 	public void shuffle() {
